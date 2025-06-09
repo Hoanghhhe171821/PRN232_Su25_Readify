dd0d0951e4f02f659b06a144d7d9f943f9e5b5b0
HoangHHHE171821 <hoanghhhe171821@fpt.edu.vn>
2025-06-09 14:43:47 +0700

    Update BooksController.cs

--- PRN232_Su25_Readify_WebAPI/Controllers/BooksController.cs
+++ PRN232_Su25_Readify_WebAPI/Controllers/BooksController.cs
@@ -1,16 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PRN232_Su25_Readify_WebAPI.Models;
-
+using PRN232_Su25_Readify_WebAPI.DbContext;
 namespace PRN232_Su25_Readify_WebAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class BooksController : Controller
     {
-        private readonly Prn232Su25FinalProjectReadifyContext _context;
+        private readonly ReadifyDbContext _context;
         private List<Book> _books;
-        public BooksController(Prn232Su25FinalProjectReadifyContext context)
+        public BooksController(ReadifyDbContext context)
         {
             _context = context;
         }
@@ -33,15 +33,15 @@ namespace PRN232_Su25_Readify_WebAPI.Controllers
         [HttpPost("{BookId}")]
         public async Task<IActionResult> UpdateBookByBookId(int BookId, [FromBody] Book updateBook)
         {
-            var book = await _context.Books.FirstOrDefaultAsync(b => b.BookId == BookId);
+            var book = await _context.Books.FirstOrDefaultAsync(b => b.Id == BookId);
 
             if (book == null) return NotFound();
 
-            book.Title = updateBook.Title;
-            book.Description = updateBook.Description;
-            book.IsFree = updateBook.IsFree;
-            book.Price = updateBook.Price;
-            book.Status = updateBook.Status;
+            //book.Title = updateBook.Title;
+            //book.Description = updateBook.Description;
+            //book.IsFree = updateBook.IsFree;
+            //book.Price = updateBook.Price;
+            //book.Status = updateBook.Status;
 
             await _context.SaveChangesAsync();
             
