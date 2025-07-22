12cd0b174a29909c45d90d070cfd8f75a565470f
TuanHungprovip <163823634+TuanHungprovip@users.noreply.github.com>
2025-07-22 23:36:41 +0700

    .
    ...

---
+++ PRN232_Su25_Readify_WebAPI/Dtos/Books/CreateBookDto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class CreateBookDto
+{
+    [Required]
+    public string Title { get; set; }
+
+    [Required]
+    public string Description { get; set; }
+
+    public bool IsFree { get; set; }
+
+    [Range(0, int.MaxValue)]
+    public int Price { get; set; }
+
+    public int UnitInOrder { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    [Range(0.0, 1.0)]
+    public decimal RoyaltyRate { get; set; }
+
+    [Required]
+    public int AuthorId { get; set; }
+
+    public string? UploadedBy { get; set; }
+
+    public DateTime CreateDate { get; set; } = DateTime.Now;
+
+    public DateTime? UpdateDate { get; set; }
+
+}
