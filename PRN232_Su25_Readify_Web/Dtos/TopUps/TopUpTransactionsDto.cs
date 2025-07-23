fb9b6793173fd0797c593f12dad2bc6db5897988
dongnuc <dongnuc2k3@gmail.com>
2025-07-23 10:52:56 +0700

    filter role,history order,topup in role user

---
+++ PRN232_Su25_Readify_Web/Dtos/TopUps/TopUpTransactionsDto.cs
@@ -0,0 +1,16 @@
+﻿namespace PRN232_Su25_Readify_Web.Dtos.TopUps
+{
+    public class TopUpTransactionsDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = null!;
+        public int Points { get; set; }
+        public int Amount { get; set; }
+        public string? MoMoOrderId { get; set; } = null!;
+        public string? MomoRequestId { get; set; } = null!;
+        public string? QrCodeUrl { get; set; } = null!;
+        public string? PaymentUrl { get; set; } = null!;
+        public string Status { get; set; } = null!;
+        public DateTime CreateDate { get; set; }
+    }
+}
