0e0b096ef006ce87aa5a6085bd63f367c86b077d
TuanHungprovip <163823634+TuanHungprovip@users.noreply.github.com>
2025-07-26 22:34:12 +0700

    ...
    ...

---
+++ PRN232_Su25_Readify_WebAPI/Migrations/20250725150528_ContributorRequest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PRN232_Su25_Readify_WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ContributorRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContributorRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FrontIdImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BackIdImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PortraitImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BankAccount = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsAgreedToPolicy = table.Column<bool>(type: "bit", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContributorRequests", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContributorRequests");
+        }
+    }
+}
